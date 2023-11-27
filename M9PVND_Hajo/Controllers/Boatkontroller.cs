423ae674d9dd20b37e689d4c1e68685d6971f056
M9PVND <szabo.marton.laszlo@gmail.com>
2023-11-27 14:41:03 +0100

    kontroller

---
+++ M9PVND_Hajo/Controllers/Boatkontroller.cs
@@ -0,0 +1,20 @@
+﻿using M9PVND_Hajo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace M9PVND_Hajo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Boatkontroller : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public IActionResult MindegyHogyHivjak()
+        {
+            HajosContext context = new HajosContext();
+            var kérdések = from x in context.Questions select x.Question1;
+
+            return Ok(kérdések);
+        }
+}
